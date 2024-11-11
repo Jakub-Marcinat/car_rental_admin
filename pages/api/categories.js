3a785cb4bcae43b29895919fe26c4c85d8db8352
Jakub-Marcinat <jakubmarcinat@gmail.com>
2024-11-11 10:08:54 +0100

    Admin authorization

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,10 +1,14 @@
 import { Category } from "@/models/Category";
 import { mongooseConnect } from "@/lib/mongoose";
 
+import { authOptions, isAdminRequest } from "./auth/[...nextauth]";
+
 export default async function handleCategories(req, res) {
+  const { method } = req;
+
   await mongooseConnect(); // Ensure MongoDB connection
 
-  const { method } = req;
+  await isAdminRequest(req, res);
 
   if (method === "GET") {
     res.json(await Category.find().populate("parent"));
